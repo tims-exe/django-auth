8992032990c4413b973260d89a464533e77dea0f
tims-exe <timonkjohn@gmail.com>
2025-01-12 23:17:17 +0530

    auth config

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,9 +4,10 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password']
+        fields = ["id", "username", "password"]
         extra_kwargs = {"password": {"write_only": True}}
-    
-    def create(seld, validate_data):
-        user = User.objects.create(**validate_data)
+
+    def create(self, validated_data):
+        print(validated_data)
+        user = User.objects.create_user(**validated_data)
         return user
