6a5f7a479120e3191919a24fe5eb92c806543988
tims-exe <timonkjohn@gmail.com>
2025-01-26 23:51:57 +0530

    backend updated

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
